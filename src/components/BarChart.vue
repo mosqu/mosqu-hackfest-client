b43cde861a62b6995857f252d13fae5b34a6b031
arifintahu <miftahul97@gmail.com>
2020-11-29 09:07:45 +0700

    add chart jamaah

---
+++ src/components/BarChart.vue
@@ -0,0 +1,19 @@
+<script>
+  import { Bar, mixins } from 'vue-chartjs';
+  const { reactiveProp } = mixins;
+
+  export default {
+    name: 'BarChart',
+    extends: Bar,
+    mixins : [reactiveProp],
+    props:['chartData','options'],
+    methods: {
+      update() {
+        this.$data._chart.update()
+      }
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options);
+    }
+  };
+</script>
