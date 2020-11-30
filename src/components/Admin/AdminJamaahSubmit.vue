24a2ff88409b2f6afa98d5d90fdbcfbd39597aea
galih.ramadhan@xtremax.com <galih.ramadhan@xtremax.com>
2020-12-01 06:43:03 +0700

    update admin page

--- src/components/Admin/AdminJamaahSubmit.vue
+++ src/components/Admin/AdminJamaahSubmit.vue
@@ -6,79 +6,29 @@
           <v-toolbar-title>Kepala Keluarga</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
-          <v-text-field outlined label="Pekerjaan"></v-text-field>
-          <v-text-field outlined label="Penghasilan"></v-text-field>
-          <v-text-field outlined label="Status Mukim"></v-text-field>
-          <v-text-field outlined label="Keahlian Khusus"></v-text-field>
+          <v-text-field outlined v-model="jamaah.name" label="Nama"></v-text-field>
+          <v-text-field outlined v-model="jamaah.address" label="Alamat"></v-text-field>
+          <v-text-field outlined v-model="jamaah.age" label="Umur"></v-text-field>
+          <v-text-field outlined v-model="jamaah.nomor_hp" label="Nomor HP"></v-text-field>
+          <v-text-field outlined v-model="jamaah.occupation" label="Pekerjaan"></v-text-field>
+          <v-text-field outlined v-model="jamaah.salary" label="Penghasilan"></v-text-field>
+          <v-text-field outlined v-model="jamaah.status_mukim" label="Status Mukim"></v-text-field>
+          <v-text-field outlined v-model="jamaah.keahlian_khusus" label="Keahlian Khusus"></v-text-field>
         </v-card-text>
       </v-card>
-      <v-card flat outlined class="ma-4">
+      <v-card v-for="member in members" :key="member" flat outlined class="ma-4">
         <v-toolbar flat color="primary white--text">
-          <v-toolbar-title>Istri</v-toolbar-title>
+          <v-toolbar-title>Anggota {{ member }}</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
-          <v-text-field outlined label="Pekerjaan"></v-text-field>
-          <v-text-field outlined label="Penghasilan"></v-text-field>
-          <v-text-field outlined label="Status Mukim"></v-text-field>
-          <v-text-field outlined label="Keahlian Khusus"></v-text-field>
-        </v-card-text>
-      </v-card>
-      <v-card flat outlined class="ma-4">
-        <v-toolbar flat color="primary white--text">
-          <v-toolbar-title>Anak 1</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
-        </v-card-text>
-      </v-card>
-      <v-card flat outlined class="ma-4">
-        <v-toolbar flat color="primary white--text">
-          <v-toolbar-title>Anak 2</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
-        </v-card-text>
-      </v-card>
-      <v-card flat outlined class="ma-4">
-        <v-toolbar flat color="primary white--text">
-          <v-toolbar-title>Anak 3</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
-        </v-card-text>
-      </v-card>
-      <v-card flat outlined class="ma-4">
-        <v-toolbar flat color="primary white--text">
-          <v-toolbar-title>Anak 4</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-text-field outlined label="Nama"></v-text-field>
-          <v-text-field outlined label="Nama Panggilan"></v-text-field>
-          <v-text-field outlined label="Alamat"></v-text-field>
-          <v-text-field outlined label="Umur"></v-text-field>
-          <v-text-field outlined label="Nomor HP"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].name" label="Nama"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].age" label="Umur"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].age" label="Status Dalam Keluarga"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].nomor_hp" label="Nomor HP"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].occupation" label="Pekerjaan"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].salary" label="Penghasilan"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].status_mukim" label="Status Mukim"></v-text-field>
+          <v-text-field outlined v-model="jamaah.member[member].keahlian_khusus" label="Keahlian Khusus"></v-text-field>
         </v-card-text>
       </v-card>
       <v-card flat outlined class="ma-4">
@@ -86,30 +36,32 @@
           <v-toolbar-title>Aktivitas</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
-          <v-text-field outlined label="Sholat"></v-text-field>
-          <v-text-field outlined label="Zakat"></v-text-field>
-          <v-text-field outlined label="Pengajian"></v-text-field>
+          <v-text-field outlined v-model="jamaah.sholat" label="Sholat"></v-text-field>
+          <v-text-field outlined v-model="jamaah.zakat" label="Zakat"></v-text-field>
+          <v-text-field outlined v-model="jamaah.pengajian" label="Pengajian"></v-text-field>
+          <v-text-field outlined v-model="jamaah.catatan" label="Catatan"></v-text-field>
         </v-card-text>
       </v-card>
+      <v-btn @click="members++" color="primary">
+        Tambahkan Anggota
+      </v-btn>
       <v-btn @click="submitJamaah" color="primary">
         Submit!
-        <v-progress-circular
-          v-if="this.$store.getters.authStatus === 'loading'"
-          class="ml-2"
-          indeterminate
-          color="secondary"
-          size="16"
-          width="2"
-        ></v-progress-circular>
       </v-btn>
     </form>
   </v-container>
 </template>
 <script>
 export default {
+  data(){
+    return{
+      jamaah: {},
+      members: 0
+    }
+  },
   methods:{
     submitJamaah(){
-      
+      console.log(this.jamaah)
     }
   }
 };
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,57 +1,64 @@
 <template>
   <div>
-    <v-container fluid pa-0 class=" fill-height" style="min-height: 600px">
-        <v-row no-gutters wrap align="start">
-          <v-col sm="12" md="6" lg="4">
-            <v-card flat class="content ma-16">
-                <div class="display-1 mb-3 font-weight-bold">
-                    Masuk
+    <v-container fluid pa-0 class="fill-height" style="min-height: 600px">
+      <v-row no-gutters wrap align="start">
+        <v-col sm="12" md="6" lg="4">
+          <v-card flat class="content ma-16">
+            <div class="display-1 mb-3 font-weight-bold">Masuk</div>
+            <div>
+              <form v-on:keyup.enter="login">
+                <v-text-field
+                  class="mt-4"
+                  outlined
+                  v-model.trim="$v.loginForm.username.$model"
+                  :error-messages="usernameError"
+                  label="Username"
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  v-model.trim="$v.loginForm.password.$model"
+                  :error-messages="passwordError"
+                  :type="showPass ? 'text' : 'password'"
+                  label="Password"
+                  :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="showPass = !showPass"
+                ></v-text-field>
+                <v-btn @click="login" color="primary">
+                  Masuk!
+                  <v-progress-circular
+                    v-if="this.$store.getters.authStatus === 'loading'"
+                    class="ml-2"
+                    indeterminate
+                    color="secondary"
+                    size="16"
+                    width="2"
+                  ></v-progress-circular>
+                </v-btn>
+                <div
+                  v-if="this.$store.getters.authStatus === 'error'"
+                  class="red--text"
+                >
+                  Username atau password salah
                 </div>
-                <div >
-                    <form v-on:keyup.enter="login">
-                      <v-text-field
-                        class="mt-4"
-                        outlined
-                        v-model.trim="$v.loginForm.username.$model"
-                        :error-messages="usernameError"
-                        label="Username"
-                      ></v-text-field>
-                      <v-text-field
-                        outlined
-                        v-model.trim="$v.loginForm.password.$model"
-                        :error-messages="passwordError"
-                        :type="showPass ? 'text' : 'password'"
-                        label="Password"
-                        :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
-                        @click:append="showPass = !showPass"
-                      ></v-text-field>
-                      <v-btn @click="login" color="primary">
-                        Masuk!
-                        <v-progress-circular
-                          v-if="this.$store.getters.authStatus === 'loading'"
-                          class="ml-2"
-                          indeterminate
-                          color="secondary"
-                          size="16"
-                          width="2"
-                        ></v-progress-circular>
-                      </v-btn>
-                      <div v-if="this.$store.getters.authStatus === 'error'" class="red--text">
-                        Username atau password salah
-                      </div>
-                    </form>
-                </div>
-            </v-card>
-          </v-col>
-          <v-col sm="12" md="6" lg="8" class="pa-10" v-if="$vuetify.breakpoint.mdAndUp">
-            <v-img
-                :src="require('../assets/illustration3.svg')"
-                max-height="560px"
-                contain
-                transition="scale-transition"
-              />
-          </v-col>
-        </v-row>
+              </form>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col
+          sm="12"
+          md="6"
+          lg="8"
+          class="pa-10"
+          v-if="$vuetify.breakpoint.mdAndUp"
+        >
+          <v-img
+            :src="require('../assets/illustration3.svg')"
+            max-height="560px"
+            contain
+            transition="scale-transition"
+          />
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
