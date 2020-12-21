7874e359d39f164dbd0f2270607800c767003427
arifintahu <miftahul97@gmail.com>
2020-12-21 20:37:47 +0700

    update menu

--- src/store/auth.js
+++ src/store/auth.js
@@ -8,28 +8,31 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     status: localStorage.getItem("status") || "",
-    token: localStorage.getItem("token") || "",
-    user: localStorage.getItem("user") || "",
+    token:  localStorage.getItem("token") || "",
+    user:   localStorage.getItem("user") || "",
     masjid: localStorage.getItem("masjid") || "",
+    menus:  localStorage.getItem("menus") || ""
   },
   mutations: {
     auth_request(state) {
       state.status = "loading";
     },
     auth_success(state, payload) {
-      state.status = "success";
-      state.token = payload.token;
-      state.user = payload.username;
-      state.masjid = payload.masjid;
+      state.status    = "success";
+      state.token     = payload.token;
+      state.user      = payload.username;
+      state.masjid    = payload.masjid;
+      state.menus     = payload.menus;
     },
     auth_error(state) {
-      state.status = "error";
+      state.status    = "error";
     },
     logout(state) {
-      state.status = "";
-      state.token = "";
-      state.user = "";
-      state.username = "";
+      state.status    = "";
+      state.token     = "";
+      state.user      = "";
+      state.username  = "";
+      state.menus     = "";
     },
   },
   actions: {
@@ -43,7 +46,7 @@ export default new Vuex.Store({
               const status = resp.data.status;
               if (status) {
                 const token = resp.data.data.token;
-                const name = user.username;
+                const name  = user.username;
                 localStorage.setItem("status", "success");
                 localStorage.setItem("token", token);
                 localStorage.setItem("user", name);
@@ -52,9 +55,14 @@ export default new Vuex.Store({
                 console.log("login success");
                 // Get masjid data
                 axios.get("/meta_").then(resp => {
-                  var masjidId = resp.data.masjid.masjid_uid;
+                  var masjidId  = resp.data.masjid.masjid_uid;
                   localStorage.setItem("masjid", masjidId)
-                  var payload = { token: token, username: name , masjid: masjidId};
+                  var payload   = { 
+                    token: token, 
+                    username: name , 
+                    masjid: masjidId,
+                    menus: resp.data.menus
+                  };
                   commit("auth_success", payload);
                   router.push("/admin");
                   resolve(resp);
@@ -110,5 +118,6 @@ export default new Vuex.Store({
     authStatus: (state) => state.status,
     getToken: (state) => state.token,
     getMasjid: (state) => state.masjid,
+    getMenus: (state) => state.menus,
   },
 });
--- src/views/Admin/AdminHome.vue
+++ src/views/Admin/AdminHome.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container>
       <v-row>
-        <v-col v-for="(item, i) in items.slice(1)" :key="i">
+        <v-col v-for="(item, i) in $store.getters.getMenus" :key="i">
           <v-card>
             <v-card-title primary-title>
               <v-icon class="mr-2">{{ item.icon }}</v-icon>
@@ -21,12 +21,12 @@
 export default {
   data() {
     return {
-      items: [
-        { title: "Beranda", icon: "mdi-view-dashboard", link: "/admin/home" },
-        { title: "Jamaah", icon: "mdi-account-box", link: "/admin/jamaah" },
-        { title: "Keuangan", icon: "mdi-cart", link: "/admin/keuangan" },
-        { title: "Kegiatan", icon: "mdi-calendar", link: "/admin/kegiatan" },
-      ],
+      // items: [
+      //   { title: "Beranda", icon: "mdi-view-dashboard", link: "/admin/home" },
+      //   { title: "Jamaah", icon: "mdi-account-box", link: "/admin/jamaah" },
+      //   { title: "Keuangan", icon: "mdi-cart", link: "/admin/keuangan" },
+      //   { title: "Kegiatan", icon: "mdi-calendar", link: "/admin/kegiatan" },
+      // ],
     };
   },
 };
