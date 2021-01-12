3c8d9fc13d4ee353a65679fe5fcdc8f86fc9f8d4
galih.ramadhan@xtremax.com <galih.ramadhan@xtremax.com>
2021-01-12 23:32:51 +0700

    update admin profile

--- src/views/Admin/AdminProfile.vue
+++ src/views/Admin/AdminProfile.vue
@@ -6,21 +6,25 @@
           <v-icon class="mr-2">mdi-account</v-icon>
           Profil
         </v-card-title>
-        <v-card flat outlined class="ma-4">
-          <v-toolbar flat color="primary white--text">
-            <v-toolbar-title>Masjid Nur Mudrikah</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-text-field
-              outlined
-              label="Nama"
-            ></v-text-field>
-            <v-text-field
-              outlined
-              label="Alamat"
-            ></v-text-field>
-          </v-card-text>
-        </v-card>
+        <v-row>
+          <v-col cols="4">
+            <v-card flat outlined class="ml-4 pa-4 fill-height">
+              <v-img class="rounded-circle mx-auto" src="https://randomuser.me/api/portraits/men/85.jpg" max-height="200" max-width="200" contain></v-img>
+              <div class="text-center mt-2 text-subtitle-1"> {{"username_placeholder"}} </div>
+            </v-card>
+          </v-col>
+          <v-col cols="8">
+            <v-card flat outlined class="mr-4">
+              <v-toolbar flat color="primary white--text">
+                <v-toolbar-title>{{"mosque_name_placeholder"}}</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-text-field outlined label="Nama Lengkap"></v-text-field>
+                <v-text-field outlined label="Email"></v-text-field>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-card>
     </v-container>
   </div>
